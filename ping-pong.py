6f6b748055d7b387d4832bcfbd7e87191442d816
Un1con <159650920+Un1con@users.noreply.github.com>
2024-02-20 23:05:28 +0200

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -13,35 +13,36 @@ game = True
 
 
 class GameSprite(sprite.Sprite):
-    
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        
-        sprite.Sprite.__init__(self)
- 
-        
-        self.image = transform.scale(
-            image.load(player_image), (size_x, size_y))
+    def __init__(self, player_image, player_x, player_y, player_speed, widht, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (widht, height))
         self.speed = player_speed
- 
-        
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
- 
-    
+        
     def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
- 
+        window.blit(self.image), (self.rect.x, self.rect.y)
 
 class Player(GameSprite):
     
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+        
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
  
+
 while game:
     
     for e in event.get():
