96c9e97cb041af2cdc4462e539dab2cac1025145
Un1con <159650920+Un1con@users.noreply.github.com>
2024-02-24 20:06:28 +0200

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -1,6 +1,7 @@
 from pygame import *
 import random
 
+init()
 win_width = 600
 win_height = 500
 display.set_caption("Shooter")
@@ -11,6 +12,8 @@ clock = time.Clock()
 fps = 60
 game = True
 
+font = font.Font(None, 60)
+
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speedX, player_speedY, widht, height):
@@ -18,7 +21,7 @@ class GameSprite(sprite.Sprite):
         self.image = transform.scale(image.load(player_image), (widht, height))
         self.speedX = player_speedX
         self.speedY = player_speedY
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect() 
         self.rect.x = player_x
         self.rect.y = player_y
         
@@ -49,13 +52,23 @@ left_platform = Player("left_platform.png", 50, 200, None, 10, 20, 100)
 
 #ball direction
 
-ball_direction_x = random.uniform(-6.5, 6.5)
-ball_direction_y = random.uniform(-6.5, 6.5)
+ball_direction_x = random.randint(-5, 5)
+ball_direction_y = random.randint(-5, 5)
 
+if ball_direction_x == 0:
+    ball_direction_x = 5
+
+if ball_direction_y == 0:
+    ball_direction_y = 5
 
 #ball
 
 ball = GameSprite("ball.png", 300, 230, ball_direction_x, ball_direction_y, 30, 30)
+
+#score
+
+left_score = 0
+right_score = 0
 
 
 while game:
@@ -70,7 +83,13 @@ while game:
     right_platform.reset()
     right_platform.update_r()
     left_platform.reset()
-    left_platform.update_l()
+    left_platform.update_l()  
+
+    r_score = font.render("{}".format(right_score), True, (0, 0, 0))
+    window.blit(r_score, (520, 20))
+
+    l_score = font.render("{}".format(left_score), True, (0, 0, 0))
+    window.blit(l_score, (70, 20))
 
     #ball
     ball.reset()
@@ -86,19 +105,45 @@ while game:
 
     if ball.rect.colliderect(left_platform.rect):
 
-        ball_direction_x = abs(ball_direction_x) + 0.7
+        ball_direction_x = abs(ball_direction_x) + 0.5
 
-        if ball_direction_y < 0:
-            ball_direction_y = abs(ball_direction_y)
-
-        if ball_direction_y > 0:
-            ball_direction_y = -ball_direction_y
         
     if ball.rect.colliderect(right_platform.rect):
-        
-        ball_direction_x = -ball_direction_x - 0.7
 
-        
+        a = random.randint(1, 2)
+
+        ball_direction_x = -ball_direction_x - 0.5
+
+        if a == 1:
+
+            ball_direction_y = abs(ball_direction_y)
+        else:
+
+            ball_direction_y = -ball_direction_y
+
+    if ball.rect.x <= 20:
+
+        right_score += 1
+
+        ball.rect.x = 300
+        ball.rect.y = 230
+        ball_direction_x = abs(ball_direction_x) - 3
+
+        if ball_direction_x == 0:
+            ball_direction_x = 2
+    
+    if ball.rect.x >= 580:
+
+        left_score += 1
+
+        ball.rect.x = 300
+        ball.rect.y = 230
+        ball_direction_x = -ball_direction_x + 3
+
+        if ball_direction_x >= 0:
+            ball_direction_x = -2
+
+    
 
     clock.tick(fps)
     display.update()
